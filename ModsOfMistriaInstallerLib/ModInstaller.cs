2300438cb2edba2ae082a42a30de6a79effb288f
DeUlo <99496313+DeUloO@users.noreply.github.com>
2026-08-20 03:37:20 +0200

    Compact furniture shadow (#165)
    Co-authored-by: AnnaNomoly <annanomoly@outlook.com>

--- ModsOfMistriaInstallerLib/ModInstaller.cs
+++ ModsOfMistriaInstallerLib/ModInstaller.cs
@@ -294,7 +294,7 @@ public class ModInstaller
         
         foreach (var generator in GetGenerators())
         {
-            generatedInformation.Merge(generator.Generate(mod));
+            generatedInformation.Merge(generator.Generate(effectiveMod));
         }
         
         generatedInformation.Merge(new TOMLCollector().Collect(effectiveMod));
